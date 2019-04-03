aaf2fe58941a3d0328f46a22cbfdcae3b6929f7b
James Phillips <JamesPhillipsUK@users.noreply.github.com>
2019-04-03 21:26:18 +0100

    Program.cs now handles arguments.  It'll handle two arguments, one for the location we want to put the documentation in, and one for the code file we want to document.

--- QuickDoc/Program.cs
+++ QuickDoc/Program.cs
@@ -2,11 +2,30 @@
 
 namespace QuickDoc
 {
-    class Program
+  class Program
+  {
+    private static void HandleArgs(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      if (args.Length != 2)
+        Console.WriteLine("Please pass a documentation location and a file to document.");
+      else
+      {
+        if (System.IO.File.Exists(args[0]))
+          Console.WriteLine("Docs File Exists.");
+        else
+          Console.WriteLine("Docs File Doesn't Exist.");
+
+        if (System.IO.File.Exists(args[1]))
+          Console.WriteLine("Code File Exists.");
+        else
+          Console.WriteLine("Code File Doesn't Exist.");
+      }
     }
+
+    static void Main(string[] args)
+    {
+      HandleArgs(args);
+      Console.ReadLine();
+    }
+  }
 }
