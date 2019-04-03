ee7f3198c7782a68c2803ad86cb5a859f09aee10
James Phillips <JamesPhillipsUK@users.noreply.github.com>
2019-04-03 18:38:42 +0100

    Added VS and VS Code files for project.

---
+++ QuickDoc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuickDoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
